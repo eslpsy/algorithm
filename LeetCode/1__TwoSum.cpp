09f46b933c40955b585b30c4d3f3204e02f92d74
eslpsy <zhangjianfeng415@gmail.com>
2017-08-14 22:54:41 +0800

    towsum

---
+++ LeetCode/1__TwoSum.cpp
@@ -0,0 +1,20 @@
+//算法时间复杂度为O(NlogN)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for (vector<int>::iterator it = nums.begin(); it != nums.end(); ++it)
+        {
+			//使用了algorithm中的std::find方法，为二分查找
+            vector<int>::iterator find_result = find(it + 1, nums.end(), target - *it);
+            if (find_result != nums.end())
+            {
+                result.push_back(it - nums.begin());
+                result.push_back(find_result - nums.begin());
+                break;
+            }
+        }
+        
+        return result;
+    }
+};
