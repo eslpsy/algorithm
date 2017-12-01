051b4ecfa9b5a9c1a739405b2dbe4ac70e177d0a
eslpsy <zhangjianfeng415@gmail.com>
2017-12-01 13:08:19 +0800

    modify stack2queue

--- STL/stack2queue.cpp
+++ STL/stack2queue.cpp
@@ -1,7 +1,7 @@
 //用两个栈来模拟队列
-//入队时直接压进栈s1 时间复杂度O(1)
-//出队时如果栈s2为空则将s1逐个抛出压入s2,然后抛出s2栈顶 时间复杂度O(n)
-//访问队头和队尾也需要考虑s1或s2栈可能为空的状态 时间复杂度O(n)
+//入队时直接压进栈push_stack 时间复杂度O(1)
+//出队时如果栈pop_stack为空则将push_stack逐个抛出压入pop_stack,然后抛出pop_stack栈顶 时间复杂度O(n)
+//访问队头和队尾也需要考虑push_stack或pop_stack栈可能为空的状态 时间复杂度O(n)
 
 #include <cstdio>
 #include <queue>
@@ -17,80 +17,77 @@ public:
 	T& back();
 
 private:
-	std::stack<T> s1;
-	std::stack<T> s2;
+	std::stack<T> push_stack;
+	std::stack<T> pop_stack;
 };
 
 template<typename T>
 void stack2queue<T>::push(const T& value)
 {
-	s1.push(value);
+	push_stack.push(value);
 }
 
 template <typename T>
 void stack2queue<T>::pop()
 {
-	if (s2.empty())
+	if (pop_stack.empty())
 	{
-		while (!s1.empty())
+		while (!push_stack.empty())
 		{
-			s2.push(s1.top());
-			s1.pop();
+			pop_stack.push(push_stack.top());
+			push_stack.pop();
 		}
 	}
 	
-	s2.pop();
+	pop_stack.pop();
 }
 
 template <typename T>
 T& stack2queue<T>::front()
 {
-	if (s2.empty())
+	if (pop_stack.empty())
 	{
-		while (!s1.empty())
+		while (!push_stack.empty())
 		{
-			s2.push(s1.top());
-			s1.pop();
+			pop_stack.push(push_stack.top());
+			push_stack.pop();
 		}
 	}
-	return s2.top();
+	return pop_stack.top();
 }
 
 template <typename T>
 T& stack2queue<T>::back()
 {
-	if (s1.empty())
+	if (push_stack.empty())
 	{
-		while (!s2.empty())
+		while (!pop_stack.empty())
 		{
-			s1.push(s2.top());
-			s2.pop();
+			push_stack.push(pop_stack.top());
+			pop_stack.pop();
 		}
 	}
-	return s1.top();
+	return push_stack.top();
 }
-
-
-
 
 int main()
 {	
-	stack2queue<int> s2q;
+	stack2queue<int> queue_entity;
 
 	for (int i = 0; i < 10; ++i)
 	{
-		s2q.push(i);
+		queue_entity.push(i);
 	}
 
-	printf("s2q front is: %d, s2q back is: %d \n", s2q.front(), s2q.back()); // 队头0, 队尾9
+	printf("queue_entity front is: %d, queue_entity back is: %d \n", queue_entity.front(), queue_entity.back()); // 队头0, 队尾9
 
-	s2q.push(10);
+	queue_entity.push(10);
 
-	printf("s2q front is: %d, s2q back is: %d \n", s2q.front(), s2q.back()); // 队头0, 队尾10
+	printf("queue_entity front is: %d, queue_entity back is: %d \n", queue_entity.front(), queue_entity.back()); // 队头0, 队尾10
 
-	s2q.pop();
+	queue_entity.pop();
 	
-	printf("s2q front is: %d, s2q back is: %d \n", s2q.front(), s2q.back()); // 队头1, 队尾10
+	printf("queue_entity front is: %d, queue_entity back is: %d \n", queue_entity.front(), queue_entity.back()); // 队头1, 队尾10
 
 	getchar();
     return 0;
